aaa8006988d1989312b3a170d22d33d5e2144cb1
ksledz <kamila.ktos.sledz@gmail.com>
2022-04-10 11:20:04 +0200

    bitvec ver

--- src/index.rs
+++ src/index.rs
@@ -1,4 +1,4 @@
-//use fid_rs::Fid;
+use fid_rs::Fid;
 // TODO bitstream representation of text
 use std::cmp::min;
 
@@ -6,7 +6,7 @@ fn rounded(a: usize, b: usize) -> usize {
     if a % b == 0 {
         return a / b;
     }
-    return ((a - (a % b)) / b) + 1;
+    ((a - (a % b)) / b) + 1
 }
 
 fn lex_num(a: &[u8]) -> usize {
@@ -23,6 +23,7 @@ fn lex_num(a: &[u8]) -> usize {
     }
     res
 }
+
 fn lex_next(a: &[u8]) -> Vec<u8>{
     let mut b = a.to_vec();
     for c in b.iter_mut().rev() {
@@ -41,13 +42,20 @@ fn lex_next(a: &[u8]) -> Vec<u8>{
     b
 }
 
+pub struct LookupTable {
+    table: Vec<usize>
+}
+
+pub struct BitVector {
+    vector: fid_rs::Fid
+}
+
 pub trait PointerArray {
     fn _get_interval(&self, a: &[u8], length: usize) -> (usize, usize);
     fn new(text_rep: &[u8], perm_array: &[usize], length: usize) -> Self;
 }
 
 // pointer_array can be either FID or LookupTable
-// TODO FID implementation
 pub struct Index<B: PointerArray> {
     text_rep: Vec<u8>,
     perm_array: Vec<usize>,
@@ -67,14 +75,8 @@ impl Index<LookupTable> {
     // TODO edge cases ?
     pub fn get_interval(&self, a: &[u8], length: usize) -> &[usize]{
         let (i0, i1) = self.pointer_array._get_interval(a, length);
-        return &self.perm_array[i0..i1];
+        &self.perm_array[i0..i1]
     }
-}
-
-
-
-pub struct LookupTable {
-    table: Vec<usize>
 }
 
 impl PointerArray for LookupTable {
@@ -82,17 +84,15 @@ impl PointerArray for LookupTable {
     fn new(text_rep: &[u8], perm_array:&[usize], length: usize) -> Self {
        let mut lut = Vec::new();
        let mut l_item = b"A".repeat(length);
-       let mut l = 0;
        for (i, item) in perm_array.iter().enumerate() {
            while text_rep[item*length..(item+1)*length] >= l_item[..] {
                lut.push(i);
                l_item = lex_next(&l_item);           
            }
        }
-       return LookupTable {
+       LookupTable {
            table: lut
-       };
-
+       }
     }
 
     fn _get_interval(&self, a: &[u8], length:usize) -> (usize, usize) {
@@ -104,8 +104,56 @@ impl PointerArray for LookupTable {
         let b_padded_index = lex_num(&b_padded);
         return (self.table[a_padded_index], self.table[b_padded_index]);
     }
-
 }
 
 
+impl Index<BitVector> {
+    pub fn new(text: &[u8], subword_len: usize) -> Self {
+        let mut perm : Vec<usize> = (0..(rounded(text.len(),subword_len))).collect();
+        perm.sort_by_key(|a| &text[a*subword_len..min((a+1)*subword_len, text.len())] );
+        return Index{
+            text_rep : text.to_vec(),
+            pointer_array: BitVector::new(text, &perm, subword_len),
+            perm_array: perm,
+        };
+    }
+    // TODO edge cases ?
+    pub fn get_interval(&self, a: &[u8], length: usize) -> &[usize]{
+        let (i0, i1) = self.pointer_array._get_interval(a, length);
+        return &self.perm_array[i0..i1];
+    }
+}
 
+impl PointerArray for BitVector {
+
+    fn new(text_rep: &[u8], perm_array:&[usize], length: usize) -> Self {
+       let mut string = String::new();
+       let mut l_item = b"A".repeat(length);
+       for(i, item) in perm_array.iter().enumerate() {
+           while text_rep[item*length..(item+1)*length] > l_item[..] {
+               string.push('1');
+               l_item = lex_next(&l_item);           
+           }
+           if text_rep[item*length..(item+1)*length] == l_item[..] {
+               string.push('0');
+           }
+       }
+       return BitVector {
+           vector: Fid::from(string.as_str()),
+       };
+    }
+    // the interval of the permutation array corresponding to w starts at position rank0(select1(i0)) + 1 and ends at position rank0(select1(i1)).
+    fn _get_interval(&self, a: &[u8], length:usize) -> (usize, usize) {
+        let mut a_padded : Vec<u8>= a.to_vec();
+        a_padded.append(&mut b"A".repeat(length - a.len()));
+        let a_padded_index = lex_num(&a_padded) as u64;
+        let mut b_padded :Vec<u8>= lex_next(a);
+        b_padded.append(&mut b"A".repeat(length - a.len()));
+        let b_padded_index = lex_num(&b_padded) as u64;
+        let a_select = self.vector.select(a_padded_index).unwrap(); //option
+        let b_select = self.vector.select(b_padded_index).unwrap();
+        let a_res = self.vector.rank0(a_select) as usize;
+        let b_res = self.vector.rank0(b_select) as usize;
+        (a_res, b_res)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod index;
 use index::Index;
+use index::LookupTable;
+use index::BitVector;
 fn main() {
-    let index = Index::new(b"ACGACGTACACGGTA", 3);
-    println!("the result is {:?}",  index.get_interval(b"ACG", 3));
+    let index = Index::<LookupTable>::new(b"ACGACGTACACGGTAACG", 3);
+    println!("the LUT result is {:?}",  index.get_interval(b"ACG", 3));
+    let index2 = Index::<BitVector>::new(b"ACGACGTACACGGTAACG", 3);
+    println!("the bitvec result is {:?}",  index2.get_interval(b"ACG", 3));
 }
